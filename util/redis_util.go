8a1da11867f82fdcd222d3bee328e987461de515
yuting <515764717@qq.com>
2022-09-01 19:30:57 +0800

    add cluster redis client

--- util/redis_util.go
+++ util/redis_util.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/go-redis/redis/v8"
 	"github.com/juqiukai/glog"
+	"strings"
 )
 
 //
@@ -23,4 +24,25 @@ func NewRedisClient(redisAddr string) (*redis.Client, error) {
 		Password: "",
 		DB:       0,
 	}), nil
+}
+
+//
+//  NewRedisClusterClient
+//  @Description:
+//  @param redisAddrs  多个用 逗号分隔
+//  @param password
+//  @return *redis.ClusterClient
+//  @return error
+//
+func NewRedisClusterClient(redisAddrs string, password string) (*redis.ClusterClient, error) {
+	if "" == redisAddrs {
+		glog.Errorf("conf - redis.addrs is blank ")
+		return nil, errors.New("redis.addr not allow blank")
+	}
+
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:    strings.Split(redisAddrs, ","),
+		Password: password,
+	})
+	return rdb, nil
 }
