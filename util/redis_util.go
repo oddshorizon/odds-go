3d5fcdc24a2a42649a1593b374b51e06f7dad76e
yuting <515764717@qq.com>
2022-09-06 17:43:15 +0800

    add redis poolSize

--- util/redis_util.go
+++ util/redis_util.go
@@ -14,15 +14,19 @@ import (
 //  @return *redis.Client
 //  @return error
 //
-func NewRedisClient(redisAddr string) (*redis.Client, error) {
+func NewRedisClient(redisAddr string, poolSize int) (*redis.Client, error) {
 	if redisAddr == "" {
 		glog.Errorf("conf - redis.addr='' ")
 		return nil, errors.New("redis.addr not allow blank")
+	}
+	if poolSize <= 0 {
+		poolSize = 1000
 	}
 	return redis.NewClient(&redis.Options{
 		Addr:     redisAddr,
 		Password: "",
 		DB:       0,
+		PoolSize: poolSize,
 	}), nil
 }
 
@@ -31,18 +35,23 @@ func NewRedisClient(redisAddr string) (*redis.Client, error) {
 //  @Description:
 //  @param redisAddrs  多个用 逗号分隔
 //  @param password
+//  @param poolSize  连接池大小
 //  @return *redis.ClusterClient
 //  @return error
 //
-func NewRedisClusterClient(redisAddrs string, password string) (*redis.ClusterClient, error) {
+func NewRedisClusterClient(redisAddrs, password string, poolSize int) (*redis.ClusterClient, error) {
 	if "" == redisAddrs {
 		glog.Errorf("conf - redis.addrs is blank ")
 		return nil, errors.New("redis.addr not allow blank")
+	}
+	if poolSize <= 0 {
+		poolSize = 1000
 	}
 
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs:    strings.Split(redisAddrs, ","),
 		Password: password,
+		PoolSize: poolSize,
 	})
 	return rdb, nil
 }
