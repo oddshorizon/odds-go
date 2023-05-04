a4b18dc3f011288eec837454d040f128fa217122
yuting <515764717@qq.com>
2023-05-04 11:09:20 +0800

    修改请求头信息

--- comm/request_header.go
+++ comm/request_header.go
@@ -16,26 +16,59 @@ type RequestHeader struct {
 	DomainId string
 	FromUid  string
 	TenantId string
+	SelfMap  map[string]string // 自定义的header信息
 }
 
+func NewRequestHeader() *RequestHeader {
+	header := new(RequestHeader)
+	header.SelfMap = make(map[string]string)
+	return header
+}
+
+// SetValue
 //
-//  ParseRequestHeader
-//  @Description: 解析出rpc请求头信息
-//  @param ctx
-//  @return *RequestHeader
+//	@Description: 设置header信息
+//	@receiver header
+//	@param key
+//	@param val
+func (header *RequestHeader) SetValue(key string, val string) {
+	header.SelfMap[key] = val
+}
+
+// GetValue
 //
+//	@Description: 获取header信息
+//	@receiver header
+//	@param key
+//	@return string
+func (header *RequestHeader) GetValue(key string) string {
+	return header.SelfMap[key]
+}
+
+// ParseRequestHeader
+//
+//	@Description: 解析出rpc请求头信息
+//	@param ctx
+//	@return *RequestHeader
 func ParseRequestHeader(ctx context.Context) *RequestHeader {
-	md, _ := metadata.FromIncomingContext(ctx)
-	domainId := util.GetValFromMd(md, DOMAIN_ID)
-	fromUid := util.GetValFromMd(md, USER_ID)
-	tenantId := util.GetValFromMd(md, TENANT_ID)
-	return &RequestHeader{DomainId: domainId, FromUid: fromUid, TenantId: tenantId}
+	header, _ := ParseRequestHeaderExt(ctx)
+	return header
 }
 
+// ParseRequestHeaderExt
+//
+//	@Description: 解析出rpc请求头信息
+//	@param ctx
+//	@return *RequestHeader
+//	@return metadata.MD
 func ParseRequestHeaderExt(ctx context.Context) (*RequestHeader, metadata.MD) {
 	md, _ := metadata.FromIncomingContext(ctx)
 	domainId := util.GetValFromMd(md, DOMAIN_ID)
 	fromUid := util.GetValFromMd(md, USER_ID)
 	tenantId := util.GetValFromMd(md, TENANT_ID)
-	return &RequestHeader{DomainId: domainId, FromUid: fromUid, TenantId: tenantId}, md
+	header := NewRequestHeader()
+	header.DomainId = domainId
+	header.FromUid = fromUid
+	header.TenantId = tenantId
+	return header, md
 }
